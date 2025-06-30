9787a5421d1c52e9ced55b5ae54f06685ad9cdf1
Tanishq1627 <tanishqbansal2711@gmail.com>
2025-06-30 23:52:49 +0530

    Update assignment4_task1.py

--- assignment4_task1.py
+++ assignment4_task1.py
@@ -1,7 +1,10 @@
 try:
     with open("output.txt", "r") as file:
+        print("Reading file content:\n")
+        line_number = 1
         for line in file:
-            print(line.strip())
+            print(f"Line {line_number}: {line.strip()}")
+            line_number += 1
 except FileNotFoundError:
-    print("Error: The file 'sample.txt' does not exist.")
+    print("Error: The file 'output.txt' does not exist.")
     
