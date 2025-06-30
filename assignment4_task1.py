2d7703a80dcf1631961cc97cfd80ada3f44cf4b3
Tanishq1627 <tanishqbansal2711@gmail.com>
2025-06-30 23:35:48 +0530

    Add files via upload

--- assignment4_task1.py
+++ assignment4_task1.py
@@ -1,6 +1,7 @@
 try:
-    with open("sample.txt", "r") as file:
+    with open("output.txt", "r") as file:
         for line in file:
             print(line.strip())
 except FileNotFoundError:
     print("Error: The file 'sample.txt' does not exist.")
+    
